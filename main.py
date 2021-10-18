1648d7a9bb143f8989c85c72c23fd89e41c03600
AGEEV Vladimir <Vladimir.AGEEV@raiffeisen.ru>
2021-10-18 18:51:01 +0300

    response.text

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from requests import HTTPError
 
 
-def print_resp(url):
+def print_simple_response(url):
     try:
         response = requests.get(url)
 
@@ -16,6 +16,23 @@ def print_resp(url):
         print('Success!')
 
 
+def print_text_from_response(url):
+    try:
+        response = requests.get(url)
+        response.encoding = 'utf-8'  # Optional: requests infers this internally
+
+
+        # если ответ успешен, исключения задействованы не будут
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Если неверно указан URL адрес
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        print(response.text)
+
+
 adress = 'https://api.github.com'
 print('Посылаем запрос на' + adress)
-print_resp(adress)
+print_simple_response(adress)
+print_text_from_response(adress)
